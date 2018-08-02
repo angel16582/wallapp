d8c5960b85d243b2e5a496259c0268b7b303b44f
AlbertoR17 <alberto9406@gmail.com>
2018-08-02 10:38:11 -0700

    aaaa
    aa

--- Vista/index.php
+++ Vista/index.php
@@ -21,7 +21,7 @@
       <div class="col-md-3 left_col">
         <div class="left_col scroll-view">
           <div class="navbar nav_title" style="border: 0;">
-            <a href="index.html" class="site_title"><i class="fa fa-shopping-cart"></i> <span>Wallapp</span></a>
+            <a href="index.php" class="site_title"><i class="fa fa-shopping-cart"></i> <span>Wallapp</span></a>
           </div>
 
           <div class="clearfix"></div>
@@ -96,7 +96,7 @@
             </div>
             <!-- /sidebar menu -->
 
-            <!-- /menu footer buttons -->
+            <!-- /menu footer buttons 
             <div class="sidebar-footer hidden-small">
               <a data-toggle="tooltip" data-placement="top" title="Settings">
                 <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
@@ -110,7 +110,7 @@
               <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
-            </div>
+            </div>-->
             <!-- /menu footer buttons -->
           </div>
         </div>
@@ -127,9 +127,9 @@
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                     <img src="images/img.jpg" alt="">John Doe
-                    <span class=" fa fa-angle-down"></span>
+                    <!-- <span class=" fa fa-angle-down"></span> -->
                   </a>
-                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                  <!-- <ul class="dropdown-menu dropdown-usermenu pull-right">
                     <li><a href="javascript:;"> Profile</a></li>
                     <li>
                       <a href="javascript:;">
@@ -139,10 +139,10 @@
                     </li>
                     <li><a href="javascript:;">Help</a></li>
                     <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                  </ul>
+                  </ul> -->
                 </li>
 
-                <li role="presentation" class="dropdown">
+                <!-- <li role="presentation" class="dropdown">
                   <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
                     <i class="fa fa-envelope-o"></i>
                     <span class="badge bg-green">6</span>
@@ -205,7 +205,7 @@
                       </div>
                     </li>
                   </ul>
-                </li>
+                </li> -->
               </ul>
             </nav>
 
