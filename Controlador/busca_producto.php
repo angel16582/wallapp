1a155a4497434775c13a9c6d9730277088b5fbe5
angel16582 <angel.16582@gmail.com>
2018-08-02 11:12:33 -0700

    busqueda incompleta

---
+++ Controlador/busca_producto.php
@@ -0,0 +1,29 @@
+<?php 
+require('conec.php');
+ 
+$codigo=$_POST['codigo'];
+$rs = mysqli_query($con,"SELECT * FROM productos where codigo=$codigo");
+    
+                       $array=array();
+                       
+                       //$i=0;
+                       while($row =mysqli_fetch_assoc($rs)){
+                          //$id =$row['Id_Producto'];
+                          $codigo =$row['codigo'];
+                          $nombre=$row['nombre'];
+                          $precio=$row['precio'];
+                          // $Des=$row['Descripcion'];
+                          // $Cat=$row['Categoria'];
+                          // $im=$row['Imagen'];
+                          //$array[$i]=$row;
+                          //$i++;
+                        //$array[] = array( 'Nombre'=> $nom,'Precio_Unitario'=> $PrecioU,'Descripcion'=> $Des);
+                      }
+                      
+                       mysqli_close($con);
+                    
+                    echo json_encode( array('nombre'=> $nombre,'precio'=> $precio));
+                   
+                  
+
+ ?>
