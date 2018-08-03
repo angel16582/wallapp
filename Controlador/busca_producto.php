6320865f4af9d58398280b032772f270fbb0473e
angel16582 <angel.16582@gmail.com>
2018-08-03 14:20:29 -0700

    oferta

--- Controlador/busca_producto.php
+++ Controlador/busca_producto.php
@@ -13,17 +13,13 @@ $rs = mysqli_query($con,"SELECT * FROM productos where codigo='$codigo'");
                           $nombre=$row['nombre'];
                           $precio=$row['precio'];
                           $gra=$row['gramaje'];
-                          // $Des=$row['Descripcion'];
-                          // $Cat=$row['Categoria'];
-                          // $im=$row['Imagen'];
-                          //$array[$i]=$row;
-                          //$i++;
-                        //$array[] = array( 'Nombre'=> $nom,'Precio_Unitario'=> $PrecioU,'Descripcion'=> $Des);
+                          $oferta=$row['oferta'];
+                      
                       }
                       
                        mysqli_close($con);
                     
-                    echo json_encode( array('nombre'=> $nombre,'precio'=> $precio,'gramaje'=> $gra,'codigo'=> $codigo));
+                    echo json_encode( array('nombre'=> $nombre,'precio'=> $precio,'gramaje'=> $gra,'codigo'=> $codigo ,'oferta'=> $oferta));
                    
                   
 
