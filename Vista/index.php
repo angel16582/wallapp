a138c1a828249bdbb779a5e52d53441c857c010c
silvino123 <36116136+silvino123@users.noreply.github.com>
2018-08-23 13:05:05 -0700

    vv

--- Vista/index.php
+++ Vista/index.php
@@ -143,13 +143,13 @@
           <div class="nav_menu">
             <nav>
               <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars">&nbspBUSCADOR</i> </a>
+                <a id="menu_toggle"><i class="fa fa-bars">&nbspWALLAPP</i> </a>
               </div>
               
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">WALLAPP
+                    <img src="images/img.jpg" alt="">
                     <!-- <span class=" fa fa-angle-down"></span> -->
                   </a>
                   <!-- <ul class="dropdown-menu dropdown-usermenu pull-right">
